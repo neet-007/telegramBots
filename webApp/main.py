f92b945f445e28bed92cdf0007b04aea16a21e4d
neet-007 <moayedeffect@gmail.com>
2024-08-02 01:09:38 +0300

    the web app is hooked the flask backend is using ngrok to fake a https

--- webApp/main.py
+++ webApp/main.py
@@ -8,16 +8,19 @@ from PIL import Image
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_API_TOKEN")
-WEBSITE_URL = os.getenv("WEBSITE_URL")
+WEBAPP_HOST = os.getenv("WEBAPP_HOST")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE) -> None:
+    if not WEBAPP_HOST or not update.message:
+        return
+    print(WEBAPP_HOST)
     """Send a message with a button that opens a the web app."""
     await update.message.reply_text(
         "Please press the button below to choose a color via the WebApp.",
         reply_markup=telegram.ReplyKeyboardMarkup.from_button(
             telegram.KeyboardButton(
                 text="Open the color picker!",
-                web_app=telegram.WebAppInfo(url="https://python-telegram-bot.org/static/webappbot"),
+                web_app=telegram.WebAppInfo(url=WEBAPP_HOST),
             )
         ),
     )
@@ -28,6 +31,7 @@ def main():
         return
 
     application = telegram.ext.Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(telegram.ext.CommandHandler("start", start))
 
     application.run_polling()
     
