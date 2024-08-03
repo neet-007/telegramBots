dee03f566acca4912a652eb48e22ca9d3f1a5176
neet-007 <moayedeffect@gmail.com>
2024-08-03 21:35:11 +0300

    wrote the basis for the line command and wrote the pen command, need to rewrite to make more orginzed

--- webApp/static/script.js
+++ webApp/static/script.js
@@ -5,18 +5,31 @@ const canvas = document.getElementById("main-canvas");
 const canvasContext = canvas.getContext("2d");
 const backgroundDiv = document.getElementById("background-div");
 const canvasRect = canvas.getBoundingClientRect();
+
+const buttonsContainer = document.getElementById("button-container");
+const menuControlButton = document.getElementById("menu-control");
 const highliteButton = document.getElementById("highlite-button");
 const eraserButton = document.getElementById("eraser-button");
+const lineButton = document.getElementById("line-button");
+const penButton = document.getElementById("pen-button");
+const ellipseButton = document.getElementById("ellipse-button");
 
+let IS_MENU_OPEN = true;
 
+menuControlButton.onclick = () => toggleCommands("menu");
 highliteButton.onclick = () => toggleCommands("highlite");
 eraserButton.onclick = () => toggleCommands("erase");
+lineButton.onclick = () => toggleCommands("line");
+penButton.onclick = () => toggleCommands("pen");
+ellipseButton.onclick = () => toggleCommands("ellipse");
+
 
 console.log(canvas)
 console.log(canvasContext)
 
 let startCoordinates = { x: 0, y: 0 };
 let prevRectCoords = { x1: 0, y1: 0, x2: 0, y2: 0 };
+let prevLineCoords = { x: 0, y: 0 };
 let rectsList = [];
 backgroundDiv.style.backgroundImage = "url(/static/uploads/uploaded_img.jpg)";
 backgroundDiv.style.backgroundSize = "cover";
@@ -28,15 +41,46 @@ backgroundDiv.style.pointerEvents = "none";
 canvas.addEventListener("mousedown", handleCanvasMouseDown);
 
 function toggleCommands(command) {
-	console.log("dsdsdasdsdsadadad")
 	if (command === "highlite") {
 		canvas.removeEventListener("mousedown", handleCanvasMouseDownErase);
 		canvas.addEventListener("mousedown", handleCanvasMouseDown);
-		return
+	} else if (command === "erase") {
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownLine);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownLineAdd);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMoveLine);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDown);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMove);
+		canvas.addEventListener("mousedown", handleCanvasMouseDownErase);
+	} else if (command === "line") {
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownErase);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDown);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMove);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMoveErase);
+		canvas.addEventListener("mousedown", handleCanvasMouseDownLine);
+	} else if (command === "pen") {
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownLine);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownLineAdd);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMoveLine);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDown);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMove);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownEnd);
+		canvas.removeEventListener("mousedown", handleCanvasMouseDownErase);
+		canvas.removeEventListener("mousemove", handleCanvasMouseMoveErase);
+		canvas.removeEventListener("mouseup", handleCanvasMouseUpErase);
+		canvas.addEventListener("mousedown", handleCanvasMouseDownPen);
+	} else if (command === "ellipse") {
+
+	} else {
+		if (IS_MENU_OPEN) {
+			buttonsContainer.style.maxHeight = `${menuControlButton.offsetHeight}px`;
+			buttonsContainer.style.overflow = "hidden";
+			IS_MENU_OPEN = false;
+			return
+		}
+		buttonsContainer.removeAttribute("maxHeight")
+		buttonsContainer.style.overflow = "visible";
+		IS_MENU_OPEN = true;
 	}
-	canvas.removeEventListener("mousedown", handleCanvasMouseDown);
-	canvas.removeEventListener("mousemove", handleCanvasMouseMove);
-	canvas.addEventListener("mousedown", handleCanvasMouseDownErase);
 }
 
 function handleCanvasMouseMove(e) {
@@ -48,16 +92,36 @@ function handleCanvasMouseMove(e) {
 	};
 	canvasContext.fillRect(prevRectCoords.x1, prevRectCoords.y1,
 		prevRectCoords.x2, prevRectCoords.y2);
+	canvasContext.globalAlpha = 1.0;
 }
 
 function handleCanvasMouseMoveErase(e) {
 	canvasContext.clearRect(e.clientX - canvasRect.left, e.clientY - canvasRect.top, 50, 50);
 }
 
+function handleCanvasMouseMoveLine(e) {
+	canvasContext.clearRect(0, prevLineCoords.y - 1, canvas.width, 2);
+	canvasContext.globalAlpha = 0.2;
+	canvasContext.lineTo(e.clientX - canvasRect.left, e.clientY - canvasRect.top);
+	canvasContext.globalAlpha = 1.0;
+	canvasContext.stroke();
+}
+
+function handleCanvasMouseMovePen(e) {
+	canvasContext.lineTo(e.clientX - canvasRect.left, e.clientY - canvasRect.top);
+	canvasContext.stroke();
+}
+
 function handleCanvasMouseUpErase(_) {
 	canvas.removeEventListener("mouseup", handleCanvasMouseUpErase);
 	canvas.removeEventListener("mousemove", handleCanvasMouseMoveErase);
 	canvas.addEventListener("mousedown", handleCanvasMouseDownErase);
+}
+
+function handleCanvasMouseUpPen(_) {
+	canvas.removeEventListener("mouseup", handleCanvasMouseUpPen);
+	canvas.removeEventListener("mousemove", handleCanvasMouseMovePen);
+	canvas.addEventListener("mousedown", handleCanvasMouseDownPen);
 }
 
 function handleCanvasMouseDownEnd(_) {
@@ -85,6 +149,35 @@ function handleCanvasMouseDownErase(e) {
 	canvas.removeEventListener("mousedown", handleCanvasMouseDownErase);
 	canvas.addEventListener("mousemove", handleCanvasMouseMoveErase);
 	canvas.addEventListener("mouseup", handleCanvasMouseUpErase);
+}
+
+function handleCanvasMouseDownLine(e) {
+	canvasContext.beginPath();
+	canvasContext.moveTo(e.clientX - canvasRect.left, e.clientY - canvasRect.top);
+	canvasContext.lineWidth = 0.4;
+	canvas.removeEventListener("mousedown", handleCanvasMouseDownLine);
+	canvas.addEventListener("mousemove", handleCanvasMouseMoveLine);
+	canvas.addEventListener("mousedown", handleCanvasMouseDownLineAdd);
+}
+
+function handleCanvasMouseDownLineAdd(e) {
+	canvasContext.clearRect(0, prevLineCoords.y - 1, canvas.width, 2);
+	prevLineCoords = { x: e.clientX - canvasRect.left, y: e.clientY - canvasRect.top };
+	canvasContext.lineTo(prevLineCoords.x, prevLineCoords.y);
+	canvasContext.stroke();
+	canvasContext.moveTo(prevLineCoords.x, prevLineCoords.y);
+	canvasContext.closePath();
+	canvas.removeEventListener("mousemove", handleCanvasMouseMoveLine);
+	canvas.removeEventListener("mousedown", handleCanvasMouseDownLineAdd);
+	canvas.addEventListener("mousedown", handleCanvasMouseDownLine);
+}
+
+function handleCanvasMouseDownPen(e) {
+	canvasContext.beginPath();
+	canvasContext.moveTo(e.clientX - canvasRect.left, e.clientY - canvasRect.top);
+	canvas.removeEventListener("mousedown", handleCanvasMouseDownPen);
+	canvas.addEventListener("mousemove", handleCanvasMouseMovePen);
+	canvas.addEventListener("mouseup", handleCanvasMouseUpPen);
 }
 
 tele.WebApp.ready();
