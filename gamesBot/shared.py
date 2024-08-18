684561bf3460c357b047bff31a3504eb3ad846fc
neet-007 <moayedeffect@gmail.com>
2024-08-18 22:41:01 +0300

    made a new game class for wilty

--- gamesBot/shared.py
+++ gamesBot/shared.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import re
 from typing import Optional, Union
 import telegram
 import telegram.ext
@@ -151,152 +153,170 @@ class Draft():
 
         return [self.players[x] for x in max_vote_ids]
 
+WILTY_ROUNDS = {
+    0:"none",
+    1:"transfers",
+    2:"managers",
+    3:"players comparisions",
+    4:"predections",
+    5:"young talents"
+}
+
 class Wilty():
-    def __init__(self, chat_id:int) -> None:
-        self.chat_id = chat_id
+    def __init__(self) -> None:
         self.num_players:int = 0
-        self.curr_mod_id:int | None = None
         self.players = dict()
         self.players_ids = []
         self.curr_mod_idx = -1
         self.curr_player_idx = -1
         self.state = 0
-        self.round_type = "none"
+        self.round_type:int = 0
         self.mod_statement = ""
         self.curr_statement = ""
-        self.statements = dict()
+        self.statements = {}
 
-    def __join_game(self, player:telegram.User):
+    def join_game(self, player: telegram.User):
         if self.state != 0:
             return False
-        if player in self.players:
+
+        if player.id in self.players:
             return False
 
-        self.players[player.id] = [player, [], 0]
+        self.players[player.id] = [player, 0]
         self.players_ids.append(player.id)
         self.num_players += 1
+
         return True
 
-    def __start_game(self):
-        if self.state != 0 or self.num_players < 3:
-            return False, None, None
+    def start_game(self):
+        if self.state != 0:
+            return False, "game error"
+
+        if self.num_players < 3:
+            return False, "num players error"
 
         shuffle(self.players_ids)
-        continue_game, new_round = self.__update_game_state()
-        if not continue_game or new_round:
-            return False, None, None
-
         self.state = 1
+        return True, ""
 
-        return True, self.players.get(self.curr_mod_id, None), self.players.get(self.players_ids[self.curr_player_idx], None)
+    def get_statements(self, player: telegram.User, statements:list[str]):
+        if self.state != 1:
+            return False, "game error"
 
-    def __add_player_statements(self, player_id:int, statements:list[str]):
-        if len(statements) != 4:
-            return False
+        if player.id in self.statements:
+            return False, "player has submited error"
+        if len(statements) != 5:
+            return False, "statements length error"
 
-        self.statements[player_id] = statements
-        return True
-
-    def __set_players_statements(self):
-        if self.state != 1 or len(self.statements) != self.num_players:
-            return False
-
-        return_val = True
-        for player, statement in self.statements.items():
-            if len(statement) != 4:
-                return_val = False
-                break
-
-            statement = [x.lower() for x in statement]
-            self.players[player][1] = statement
-
-        if return_val:
+        statements = [x.strip().lower() for x in statements]
+        self.statements[player.id] = statements
+        
+        if len(self.statements) == self.num_players:
             self.state = 2
-        return return_val
+            return True, "start game"
 
-    def __update_game_state(self):
-        if self.curr_mod_idx == -1:
-            self.curr_mod_idx = self.num_players - 1
-            self.curr_player_idx = 0
-            self.round_type = ""
-            return True, False
+        return True, ""
 
-        if self.curr_player_idx == self.num_players - 1 or (self.curr_player_idx == self.num_players - 2 and self.curr_mod_idx == self.num_players - 1):
-            if self.round_type == "":
-                return False, True
+    def start_round(self):
+        if self.state != 2:
+            return False, "game error"
 
-            self.curr_mod_idx -= 1
-            if self.curr_mod_idx < 0:
-                self.curr_mod_idx = self.num_players - 1
-
-            self.curr_player_idx = 0
-            if self.curr_mod_idx == 0:
+        if self.round_type == 0:
+            self.curr_mod_idx = 0
+            self.curr_player_idx = 1
+            self.round_type = 1
+        else:
+            self.curr_mod_idx = self.curr_mod_idx + 1 % (self.num_players - 1)
+            if self.curr_mod_idx == 1:
+                self.curr_player_idx = 0
+            else:
                 self.curr_player_idx = 1
 
-            if self.round_type == "":
-                self.round_type = ""
-            elif self.round_type == "":
-                self.round_type = ""
-            elif self.round_type == "":
-                self.round_type = ""
+        self.state = 3
+        return True, ""
 
-            return True, True
-
-        self.curr_player_idx += 1
-        if self.curr_player_idx == self.curr_mod_idx:
-            self.curr_player_idx += 1
-
-        return True, False
-
-    def __start_round(self):
-        if self.state != 2:
-            return False, "", None, None
-        state, _ = self.__update_game_state()
-        if state:
-            self.state = 3
-        return state, self.round_type, self.players.get(self.curr_mod_id, None), self.players.get(self.players_ids[self.curr_player_idx], None)
-    
-    def __set_mod_state(self,mode_statement:str):
+    def get_mod_statement(self, player:telegram.User, statement:str):
         if self.state != 3:
-            return
+            return False, "game error"
+
+        if player.id != self.players_ids[self.curr_mod_idx]:
+            return False, "curr mod error"
+
+        statement = statement.strip().lower()
+        if statement == "__same__":
+            self.curr_statement = self.statements[self.players_ids[self.curr_player_idx]][self.round_type]
+        else:
+            self.curr_statement = statement
+
         self.state = 4
-        self.mod_statement = mode_statement.lower()
+        return True, ""
 
-    def __end_round(self, votes) -> tuple[bool, bool, str]:
+    def play(self, vote:bool):
         if self.state != 4:
-            return False, False, "end_game"
-    
-        return_val = True, True, ""
-        if votes[0] > votes[1]:
-            if self.mod_statement == self.curr_statement:
-                return_val =  True, False
-            else:
-                return_val = True, True
-        else:
-            if self.mod_statement == self.curr_statement:
-                return_val = True, True
-            else:
-                return_val = True, False
-    
-        if return_val[1]:
-            for id in self.players_ids:
-                if id == self.players_ids[self.curr_player_idx] or id == self.curr_mod_id:
-                    continue
-                self.players[id][2] += 1
-        else:
-            self.players[self.curr_player_idx][2] += 1
+            return False, "game error"
 
-        continue_game, round_type_end = self.__update_game_state()
-        if not continue_game:
-            return_val += tuple(["end_game"])
-        if round_type_end:
-            return_val += tuple(["end_round_type"])
-        if len(return_val) < 3:
-            return_val += tuple(["continue_round"])
-        return return_val
+        if vote:
+            if self.curr_statement != self.statements[self.players_ids[self.curr_player_idx]][self.round_type]:
+                self.players[self.players_ids[self.curr_player_idx]][1] += 1
+            else:
+                for id, player in self.players:
+                    if id == self.players_ids[self.curr_player_idx] or id == self.players_ids[self.curr_mod_idx]:
+                        continue
+                    player[1] += 1
+        else:
+            if self.curr_statement == self.statements[self.players_ids[self.curr_player_idx]][self.round_type]:
+                self.players[self.players_ids[self.curr_player_idx]][1] += 1
+            else:
+                for id, player in self.players:
+                    if id == self.players_ids[self.curr_player_idx] or id == self.players_ids[self.curr_mod_idx]:
+                        continue
+                    player[1] += 1
 
-    def __get_scores(self):
-        return {id:data[2] for id, data in self.players.items()}
+        self.curr_player_idx = self.curr_player_idx + 1 % (self.num_players - 1) 
+        if self.curr_mod_idx == 1:
+            if self.curr_player_idx == 0:
+                self.state = 5
+                return True, "end round"
+
+            return True, "continue"
+
+        if self.curr_player_idx == 1:
+            self.state = 5
+            return True, "end round"
+    
+        return True, "continue"
+
+
+    def end_round(self):
+        if self.state != 5:
+            return False, "game error"
+
+        if self.round_type == 5:
+            self.state = 6
+            return True, "end game"
+
+        self.round_type += 1
+        self.state = 2
+        return True, "next round"
+
+    def end_game(self):
+        if self.state != 6:
+            return "", []
+
+        winners = []
+        max_score = float("-inf")
+        text = ""
+        for player in self.players.values():
+            if player[1] > max_score:
+                max_score = player[1]
+                winners.clear()
+                winners.append(player[0])
+            elif player[1] == max_score:
+                winners.append(player[0])
+
+            text += f"{player[0].mention_html()}:{player[1]}\n"
+
+        return text, winners
 
 PLAYERS_COUNT = 2
 
